7b9bec46cf9a2995849ff8049473ae6059329160
Leo Petersberg <leo@leopebe.de>
2022-12-01 02:53:51 +0100

    CamVarMgmt ergänzt

--- src/main/java/org/example/CamVariableMgmt.java
+++ src/main/java/org/example/CamVariableMgmt.java
@@ -1,9 +1,17 @@
 package org.example;
 
 import org.camunda.bpm.engine.delegate.VariableScope;
+import org.camunda.spin.Spin;
+import org.camunda.spin.impl.json.jackson.JacksonJsonNode;
+import org.camunda.spin.json.SpinJsonNode;
+import spinjar.com.fasterxml.jackson.databind.JsonNode;
+import spinjar.com.fasterxml.jackson.databind.node.ArrayNode;
+import spinjar.com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import spinjar.com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 public class CamVariableMgmt {
 
@@ -20,9 +28,107 @@ public class CamVariableMgmt {
 
             //Anzahl Trainings in Camunda Variable speichern
             variableScope.setVariable("anzahlTrainings", anzahlTrainings);
+
+            updateZusammenfassung(variableScope, Prozess_ID);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void updateZusammenfassung(VariableScope variableScope, String Prozess_ID)throws Exception{
+        //Vorname
+        //Nachname
+        //Größe
+        //Geburtsdatum
+        //Geschlecht
+        //Trainingsvariante_aktuell
+
+        String sqlTeilnehmer = "SELECT Vorname, Nachname, Groesse_cm, Geburtsdatum, Geschlecht, Trainingsvariante_aktuell\n" +
+                "               FROM Prozess\n" +
+                "               Join Kunde K on Prozess.Kunden_ID = K.Kunden_ID\n" +
+                "               WHERE Prozess_ID = ?";
+        PreparedStatement stmtTeilnehmer = DBConnection.getConnection().prepareStatement(sqlTeilnehmer);
+        stmtTeilnehmer.setString(1, Prozess_ID);
+        ResultSet rsTeilnehmer = stmtTeilnehmer.executeQuery();
+        rsTeilnehmer.next();
+
+        String vorname = rsTeilnehmer.getString("Vorname");
+        String nachname = rsTeilnehmer.getString("Nachname");
+        int groesse = rsTeilnehmer.getInt("Groesse_cm");
+        String geburtsdatum = rsTeilnehmer.getString("Geburtsdatum");
+        String geschlecht = rsTeilnehmer.getString("Geschlecht");
+        String trainingsvariante = rsTeilnehmer.getString("Trainingsvariante_aktuell");
+
+        ObjectNode objectNode = JsonNodeFactory.instance.objectNode();
+        objectNode.put("vorname", vorname);
+        objectNode.put("nachname", nachname);
+        objectNode.put("groesse", groesse);
+        objectNode.put("geburtsdatum", geburtsdatum);
+        objectNode.put("geschlecht", geschlecht);
+        objectNode.put("trainingsvariante", trainingsvariante);
+
+        ArrayNode nodesUntersuchungen = objectNode.putArray("untersuchungen");
+
+        String sqlUntersuchungen = "SELECT Typ, Testtag_zeit, Gewicht_kg, Skelettmuskelmasse_kg, Koerperfettmassekg, BMI, KoerperfettProz\n" +
+                "                   FROM Untersuchung\n" +
+                "                   WHERE Prozess_ID = ?";
+        PreparedStatement stmtUntersuchungen = DBConnection.getConnection().prepareStatement(sqlUntersuchungen);
+        stmtUntersuchungen.setString(1, Prozess_ID);
+        ResultSet rsUntersuchungen = stmtUntersuchungen.executeQuery();
+        while (rsUntersuchungen.next()){
+            String typ = rsUntersuchungen.getString("Typ");
+            String testtag_zeit = rsUntersuchungen.getString("Testtag_zeit");
+            double gewicht = rsUntersuchungen.getDouble("Gewicht_kg");
+            double skelettmuskelmasse = rsUntersuchungen.getDouble("Skelettmuskelmasse_kg");
+            double koerperfettmasse = rsUntersuchungen.getDouble("Koerperfettmassekg");
+            double bmi = rsUntersuchungen.getDouble("BMI");
+            double koerperfettProz = rsUntersuchungen.getDouble("KoerperfettProz");
+
+            ObjectNode jsonNodeUntersuchung = JsonNodeFactory.instance.objectNode();
+            jsonNodeUntersuchung.put("typ", typ);
+            jsonNodeUntersuchung.put("testtag_zeit", testtag_zeit);
+            jsonNodeUntersuchung.put("gewicht", gewicht);
+            jsonNodeUntersuchung.put("skelettmuskelmasse", skelettmuskelmasse);
+            jsonNodeUntersuchung.put("koerperfettmasse", koerperfettmasse);
+            jsonNodeUntersuchung.put("bmi", bmi);
+            jsonNodeUntersuchung.put("koerperfettProz", koerperfettProz);
+
+            nodesUntersuchungen.add(jsonNodeUntersuchung);
+        }
+
+
+        ArrayNode nodesTrainings = objectNode.putArray("trainings");
+
+        String sqlSelectTrainings = "SELECT Trainingsdatum, Trainingsvariante, Unterschrift_Trainer, Unterschrift_Kunde " +
+                                    "FROM Training "+
+                                    "WHERE Prozess_ID = ?";
+        PreparedStatement stmtSelectTrainings = DBConnection.getConnection().prepareStatement(sqlSelectTrainings);
+        stmtSelectTrainings.setString(1, Prozess_ID);
+        ResultSet rsSelectTrainings = stmtSelectTrainings.executeQuery();
+        while (rsSelectTrainings.next()){
+            String trainingsdatum = rsSelectTrainings.getString("Trainingsdatum");
+            String trainingsvarianteTraining = rsSelectTrainings.getString("Trainingsvariante");
+            String unterschrift_trainer = rsSelectTrainings.getString("Unterschrift_Trainer");
+            String unterschrift_kunde = rsSelectTrainings.getString("Unterschrift_Kunde");
+
+            ObjectNode jsonNodeTraining = JsonNodeFactory.instance.objectNode();
+            jsonNodeTraining.put("trainingsdatum", trainingsdatum);
+            jsonNodeTraining.put("trainingsvariante", trainingsvarianteTraining);
+            jsonNodeTraining.put("unterschrift_trainer", unterschrift_trainer);
+            jsonNodeTraining.put("unterschrift_kunde", unterschrift_kunde);
+
+            nodesTrainings.add(jsonNodeTraining);
+        }
+
+        SpinJsonNode spinJsonNode = Spin.JSON(objectNode.toString());
+        variableScope.setVariable("zusammenfassung", spinJsonNode);
+
+
+        //Datum
+        //Trainingsvariante
+        //Unterschrift Trainer
+        //Unterschrift Kunde
     }
 
 }
